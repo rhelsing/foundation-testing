05f0725e53a42e5ad6f629778a1b22f8fbc55ef5
Ryan Helsing <ryanhelsing@gmail.com>
2014-10-25 10:47:47 -0400

    removed posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   resources :sections
 
-  resources :posts
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
